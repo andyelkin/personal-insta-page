f735d704bb1df5c396e7b1af03b800af217aabff
Andy Elkin <aelkin@i.ua>
2018-11-02 16:20:59 +0200

    2-е ДЗ
    Добавлен слайдер фотографий и изменен фон страницы.

---
+++ docs/js/slideshow.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+	var slideIndex = 0;
+
+	showPhotos();
+
+	function showPhotos(){
+
+		var i;
+		var slides = $('.photo img'); 
+		
+
+		for (i = 0; i < slides.length; i++) {
+			$(slides[i]).hide();
+		}
+
+		slideIndex ++; 
+
+		if(slideIndex > slides.length){
+			slideIndex = 1;
+		}
+
+		$(slides[slideIndex-1]).show(); 
+
+		setTimeout(showPhotos, 3000);
+
+	}
+});
