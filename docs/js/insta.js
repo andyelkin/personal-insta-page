44c0065eee6082d69c432f5b2c3377ce8b8db1e0
Andy Elkin <aelkin@i.ua>
2018-11-03 22:12:17 +0200

    3-е ДЗ
    Добавлены фотографии со страницы социальной сети Инстраграм

--- docs/js/insta.js
+++ docs/js/insta.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){ 
 
 
+    var userFeed = new Instafeed({
+        get: 'user',
+        userId: '673796884',
+        accessToken: '673796884.1677ed0.6a310de5d6614b248a57d3c8d7f5f110', 
+        resolution: 'standard_resolution', 
+        limit: 10
+    });
+    userFeed.run();
+
+
+    setTimeout(function(){
+    	$('.instafeed').addClass('shadowed');
+    }, 100);
 
 });
